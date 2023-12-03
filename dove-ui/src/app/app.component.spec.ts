3d07ec949c3c91c70a2ae2c2a770e994c73169e1
Valentin Hegedűs <valentin.vio.dev@gmail.com>
2023-12-03 19:29:00 +0100

    Updated to dove

--- dove-ui/src/app/app.component.spec.ts
+++ dove-ui/src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'osd_viewer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('osd_viewer');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('osd_viewer app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dove_app app is running!');
   });
 });
