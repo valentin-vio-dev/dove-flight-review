0ae0a40d50458ca3fd6d09019036bfab1da469dd
Valentin Hegedűs <valentin.vio.dev@gmail.com>
2023-12-04 13:41:14 +0100

    Fixed raw data overflow issue

--- dove-ui/src/app/app.component.spec.ts
+++ dove-ui/src/app/app.component.spec.ts
@@ -18,6 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dove_app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dove-ui app is running!');
   });
 });
